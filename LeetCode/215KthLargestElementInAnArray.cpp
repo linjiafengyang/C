c20dadb0810983ea83eb5dd219b917a2386661dd
linjiafengyang <1498772665@qq.com>
2017-12-24 23:30:20 +0800

    Easy

---
+++ LeetCode/215KthLargestElementInAnArray.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	int findKthLargest(vector<int>& nums, int k) {
+		sort(nums.begin(), nums.end());
+		return nums[nums.size() - k];
+	}
+};
